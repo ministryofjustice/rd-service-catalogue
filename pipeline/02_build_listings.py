e2d194fc1349611dd694624756fdc34c615851ac
Richard Leyshon <richard.leyshon@justice.gov.uk>
2024-09-25 12:07:51 +0100

    feat: Script builds listings files

---
+++ pipeline/02_build_listings.py
@@ -0,0 +1,21 @@
+import dotenv
+import pandas as pd
+from pyprojroot import here
+
+from ai_nexus.build_yaml import build_listings_from_parquet
+
+# configure secrets -------------------------------------------------------
+
+secrets = dotenv.dotenv_values(".env")
+org_nm1 = secrets["ORG_NM1"]
+org_nm2 = secrets["ORG_NM2"]
+
+for nm in [org_nm1, org_nm2]:
+    in_pth = f"data/{nm}.parquet"
+    out_pth = f"listings/{nm}.yaml"
+
+    build_listings_from_parquet(
+        prq_pth=here(in_pth),
+        template_pth=here("template.txt"),
+        yaml_out_pth=here(out_pth)
+        )
