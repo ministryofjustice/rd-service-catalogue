7166442bd722088e80ea1378b61db94c8af79f25
Richard Leyshon <richard.leyshon@justice.gov.uk>
2024-10-04 07:00:12 +0100

    test: Update retry strategy

--- tests/test_confluence_api.py
+++ tests/test_confluence_api.py
@@ -1,6 +1,7 @@
 """Testing integration with Confluence API."""
 import pytest
 from requests import Session
+from requests.adapters import HTTPAdapter, Retry
 
 from ai_nexus.confluence_api import ConfluenceClient
 
@@ -42,7 +43,7 @@ class TestConfluenceClient:
             )
     
 
-    def test_confluence_client__url_defence(self, mock_creds):
+    def test__url_defence(self, mock_creds):
         """Test defensive utility raises as expected."""
         client = ConfluenceClient(
             mock_creds["MOCK_EMAIL"],
@@ -57,4 +58,18 @@ class TestConfluenceClient:
             client._url_defence(url=False)
         with pytest.raises(TypeError, match=".* found <class 'NoneType'>"):
             client._url_defence(url=None)
-            
+    
+
+    def test__configure_atlassian(self, mock_creds):
+        """Check that default requests session can be reconfigured."""
+        client = ConfluenceClient(
+            mock_creds["MOCK_EMAIL"],
+            mock_creds["MOCK_PAT"],
+            mock_creds["MOCK_AGENT"],
+        )
+        new_sess = Session()
+        new_sess.mount("https://", Retry(total=100))
+        client._configure_atlassian(_session=new_sess)
+        assert client._session.adapters[
+            "https://"
+            ].total == new_sess.adapters["https://"].total
