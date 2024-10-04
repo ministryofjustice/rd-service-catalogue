2f2717b94bf38b8c148450f9b6c17a8d36fb3bfc
Richard Leyshon <richard.leyshon@justice.gov.uk>
2024-10-04 06:00:09 +0100

    test: Property values are passed to request Session

--- tests/test_confluence_api.py
+++ tests/test_confluence_api.py
@@ -1,5 +1,6 @@
 """Testing integration with Confluence API."""
 import pytest
+from requests import Session
 
 from ai_nexus.confluence_api import ConfluenceClient
 
@@ -27,3 +28,15 @@ class TestConfluenceClient:
         assert hasattr(client, "_ConfluenceClient__agent")
         assert hasattr(client, "_ConfluenceClient__email")
         assert hasattr(client, "_ConfluenceClient__pat")
+        assert hasattr(client, "_session")
+        assert client._ConfluenceClient__agent == mock_creds["MOCK_AGENT"]
+        assert client._ConfluenceClient__email == mock_creds["MOCK_EMAIL"]
+        assert client._ConfluenceClient__pat == mock_creds["MOCK_PAT"]
+        assert isinstance(client._session, Session)
+        assert client._session.headers["User-Agent"] == mock_creds[
+            "MOCK_AGENT"
+            ]
+        assert client._session.headers["Accept"] == "application/json"
+        assert client._session.auth == (
+            mock_creds["MOCK_EMAIL"], mock_creds["MOCK_PAT"]
+            )
