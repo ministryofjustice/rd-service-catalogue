577736c3d7e4cdcb87f401b7730e87eccff5c92d
Richard Leyshon <richard.leyshon@justice.gov.uk>
2024-12-10 12:58:11 +0000

    refactor: Pipeline uses latest API

--- pipeline/01_gulp_data.py
+++ pipeline/01_gulp_data.py
@@ -5,7 +5,7 @@ from ai_nexus_backend.github_api import GithubClient
 
 # set to True for chatty outputs
 debug = False
-# configure secrets -----------------------------------------------------------
+# configure secrets -------------------------------------------------------
 
 secrets = dotenv.dotenv_values(".env")
 user_agent = secrets["AGENT"]
@@ -15,32 +15,30 @@ org_nm2 = secrets["ORG_NM2"]
 
 client = GithubClient(github_pat=pat, user_agent=user_agent)
 
-# gulp data -------------------------------------------------------------------
-
-for nm in [org_nm1, org_nm2]:
+# gulp data ---------------------------------------------------------------
+# reversing order for troubleshooting purposes
+for nm in [org_nm2, org_nm1]:
     repos = client.get_org_repos(
         org_nm=nm,
         public_only=True,
         debug=debug,
     )
 
-    custom_props = client.get_all_org_repo_metadata(
+    custom_props = client.get_all_repo_metadata(
+        html_urls=repos["html_url"],
         metadata="custom_properties",
-        repo_nms=repos["name"],
-        org_nm=nm,
     )
 
-    topics = client.get_all_org_repo_metadata(
+    topics = client.get_all_repo_metadata(
+        html_urls=repos["html_url"],
         metadata="topics",
-        repo_nms=repos["name"],
-        org_nm=nm,
     )
 
-    # join tables -------------------------------------------------------------
+    # join tables ---------------------------------------------------------
     for tab in [repos, custom_props, topics]:
         tab.set_index("repo_url", inplace=True)
     out = repos.join(custom_props).join(topics)
 
-    # write parquet -----------------------------------------------------------
+    # write parquet -------------------------------------------------------
     out_pth = f"data/{nm}.parquet"
     out.to_parquet(here(out_pth))
